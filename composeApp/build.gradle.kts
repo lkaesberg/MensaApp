da2f692a8dad0f699e56817e380197a97972d7a8
larskaesberg <larskaesberg@gmail.com>
2025-12-17 10:21:29 +0100

    feat: update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = "com.lkaesberg.mensaapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     packaging {
         resources {
