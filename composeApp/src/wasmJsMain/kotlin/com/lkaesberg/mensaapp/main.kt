452a8af31d880132e5452810e39d04bf7f99dfc9
Jan Philip Wahle <jan.philip.wahle@gmail.com>
2026-06-03 12:28:48 +0200

    feat: add app download banner on web

--- composeApp/src/wasmJsMain/kotlin/com/lkaesberg/mensaapp/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/lkaesberg/mensaapp/main.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.w3c.dom.HTMLElement
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 fun main() {
     document.body?.style?.setProperty("touch-action", "pan-y")
-    ComposeViewport(document.body!!) {
+    // Render into #app-container so the optional download banner can sit above the canvas.
+    val container = document.getElementById("app-container") as HTMLElement
+    ComposeViewport(container) {
         App()
     }
 }
